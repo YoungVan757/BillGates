1516388371ec8470bfd9091e268203bbcb75aec1
Van <crackhead@Childs-MBP.home>
2018-10-18 14:48:13 -0400

    Updates

---
+++ precache-manifest.25ac2fa800647d9e04eeeb0bf87fa5f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96f6e7ddda7156dd0bd1",
+    "url": "/BillGates/static/js/runtime~main.96f6e7dd.js"
+  },
+  {
+    "revision": "c6122996776e548d4901",
+    "url": "/BillGates/static/js/main.c6122996.chunk.js"
+  },
+  {
+    "revision": "ccf717e8f1c052517a1a",
+    "url": "/BillGates/static/js/1.ccf717e8.chunk.js"
+  },
+  {
+    "revision": "c6122996776e548d4901",
+    "url": "/BillGates/static/css/main.d3a5c36e.chunk.css"
+  },
+  {
+    "revision": "54899bc8baab05a9dd13838cb0ac3d8b",
+    "url": "/BillGates/index.html"
+  }
+];
